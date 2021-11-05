d79fbbc64e8b4f39e87f467a90d096f9d10d7a52
maraoz <gh@maraoz.com>
2021-11-05 00:11:21 -0500

    use jpg

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 
+from PIL import Image
+
 
 def main():
     for count, filename in enumerate(os.listdir("raw")):
@@ -11,10 +13,14 @@ def main():
         zoom = 6
         pad = 30 #(2**zoom) / 2
         x, y = tuple(int(d) for d in filename.split(".")[0].split(","))
-        dst = str(int(pad+(x//n))) + "," + str(int(pad-(y//n))) + ".png"
+        dst = str(int(pad+(x//n))) + "," + str(int(pad-(y//n))) + ".jpg"
         src = 'raw/' + filename
+        im = Image.open(src)
+        
         dst = 'map/latest/' + str(zoom) + "/" + dst
         print(src, "->", dst)
-        shutil.copy(src, dst)
+
+        im.save(dst)
+
 if __name__ == '__main__':
     main()
