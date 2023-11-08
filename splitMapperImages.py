558e50d47e7ffa19c69587207db1cda881faaea9
lvanm <ivan.marienhoff@gmail.com>
2023-11-07 21:25:42 -0300

    Add Mapper Image Splitter Script
    Each image obtained from the mapper consists of a grid with 5x5 parcels. The 'SplitMapperImages.py' script splits these images to produce individual images for each parcel.

---
+++ splitMapperImages.py
@@ -0,0 +1,58 @@
+import os
+from PIL import Image
+
+input_directory = 'testImagesInput'
+output_directory = 'testImagesOutput'
+
+def split_image(img_path, output_dir):
+    try:
+        # Parse the image name to get the central coordinates
+        base_name = os.path.basename(img_path)
+        base_name = os.path.splitext(base_name)[0]
+        central_x, central_y = map(int, base_name.split(','))
+
+        # Open the image
+        img = Image.open(img_path)
+        img_width, img_height = img.size
+
+        # Size of each parcel
+        parcel_width = img_width // 5
+        parcel_height = img_height // 5
+
+        # Make sure the output directory exists
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        # Cut the image and save each parcel
+        for i in range(5):
+            for j in range(5):
+                left = j * parcel_width
+                upper = i * parcel_height
+                right = left + parcel_width
+                lower = upper + parcel_height
+                parcel_img = img.crop((left, upper, right, lower))
+
+                # Calculate the coordinates for each parcel
+                parcel_x = central_x - 2 + j
+                parcel_y = central_y + 2 - i
+
+                # Save the parcel image with the new name
+                parcel_name = f'{parcel_x},{parcel_y}.jpg'
+                parcel_path = os.path.join(output_dir, parcel_name)
+                parcel_img.save(parcel_path)
+
+        print(f'Finished processing {img_path}')
+
+    except Exception as e:
+        print(f'Error processing {img_path}: {e}')
+
+# Main function to process all images in a directory
+def process_images(input_dir, output_dir):
+    for filename in os.listdir(input_dir):
+        if filename.endswith('.jpg'):
+            img_path = os.path.join(input_dir, filename)
+            split_image(img_path, output_dir)
+
+
+# Run the main function
+process_images(input_directory, output_directory)
